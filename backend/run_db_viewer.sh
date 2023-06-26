0c91ca13b8424a96b9ac48effdcbd11a82ee34eb
nbeny <nbeny42@outlook.fr>
2023-06-26 15:59:45 +0200

    add

--- backend/run_db_viewer.sh
+++ backend/run_db_viewer.sh
@@ -2,6 +2,6 @@
 docker run --rm -d --net=host \
     --name prototype_db_viewer \
     -e PGADMIN_DEFAULT_EMAIL=admin@pgadmin.fr \
-    -e PGADMIN_DEFAULT_PASSWORD=admin \
+    -e PGADMIN_DEFAULT_PASSWORD=alesio123442 \
     -e PGADMIN_LISTEN_PORT=5555 \
     dpage/pgadmin4:latest
